de453ddad3edd23345975f139ec1990fc79cbfc0
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-12-06 10:35:21 -0600

    adds adding/displaying functionality; still need to delete/mark as complete

--- to-do-list/FirstViewController.swift
+++ to-do-list/FirstViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+ 
+    @IBOutlet var todoListTable: UITableView!
+    var todos: [String] = []
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(todos.count)
+        return todos.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "todoCell")
+        
+        cell.textLabel?.text = todos[indexPath.row]
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("view did appear")
+        let currentTodos = UserDefaults.standard.object(forKey: "todos")
+        
+        if let tempTodos = currentTodos as? [String] {
+            todos = tempTodos
+        }
+        todoListTable.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
--- to-do-list/SecondViewController.swift
+++ to-do-list/SecondViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet var todoTextField: UITextField!
+    @IBOutlet var warningLabel: UILabel!
+    
+    @IBAction func addButton(_ sender: Any) {
+        let newTodo = todoTextField.text
+        let currentTodos = UserDefaults.standard.object(forKey: "todos")
+        var todos: [String]
+        
+        if newTodo != nil && newTodo != "", let todo = newTodo {
+            if let tempTodos = currentTodos as? [String] {
+                todos = tempTodos
+                todos.append(todo)
+            } else {
+                todos = [newTodo!]
+            }
+            
+            UserDefaults.standard.set(todos, forKey: "todos")
+            updateLabel("\(todo) added to list.", withColor: UIColor.black)
+        } else {
+            updateLabel("You must enter a valid todo item.", withColor: UIColor.red)
+        }
+        
+        todoTextField.text = ""
+    }
+    
+    func updateLabel(_ labelString: String, withColor color: UIColor) {
+        warningLabel.text = labelString
+        warningLabel.textColor = color
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        warningLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        todoTextField.resignFirstResponder()
+        return true
+    }
 
 
 }
-
